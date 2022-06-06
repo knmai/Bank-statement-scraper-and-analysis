374a2e1bb61a83ca8b12e955069b04d52e3da6f9
knmai <95066293+knmai@users.noreply.github.com>
2022-06-06 13:42:30 -0500

    Code progress

--- PDF scaper.py
+++ PDF scaper.py
@@ -10,57 +10,57 @@ from dateutil.parser import parse #Fixing the dates
 #Set working directory
 os.chdir('c:\\Users\\karln\\iCloudDrive\\DataScience Projects\\Chime statements')
 
-def create_df_convert_csv(input):
-    if not sys.warnoptions:
-        import warnings
-        warnings.simplefilter("ignore")
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
 
 
-    #table = camelot.read_pdf(november, pages='all')
-    #print("Total tables extracted: ", table.n)
+#table = camelot.read_pdf(november, pages='all')
+#print("Total tables extracted: ", table.n)
 
-    def clean_trns_desc(text):
-        test = text.lower()
-        #removing anything within square brackets
-        text = re.sub('\[.*?#-\]', '',text) #The '\' starts  the string match, and currently, its matching every symbol that's in the square bracket (?*.\)
-        #if any of these punctuation marks in (string.punctuation) get rid of it
-        text = re.sub('[%s]' % re.escape(string.punctuation),'',text)
-        #getting rid of all the numbers
-        text = re.sub('\d+','',text)
-        return text
+def clean_trns_desc(text):
+    test = text.lower()
+    #removing anything within square brackets
+    text = re.sub('\[.*?#-\]', '',text) #The '\' starts  the string match, and currently, its matching every symbol that's in the square bracket (?*.\)
+    #if any of these punctuation marks in (string.punctuation) get rid of it
+    text = re.sub('[%s]' % re.escape(string.punctuation),'',text)
+    #getting rid of all the numbers
+    text = re.sub('\d+','',text)
+    return text
 
 
-    round1 = lambda x: clean_trns_desc(x)
+round1 = lambda x: clean_trns_desc(x)
 
-    try:
-        df_list = tabula.read_pdf(input, stream = True, guess = True, pages = 'all',
-                                multiple_tables=True,
-                                pandas_options={'header':None} 
-                                )
-    except Exception as e: 
-        print('The error is', e)
+try:
+    df_list = tabula.read_pdf('december21.pdf', stream = True, guess = True, pages = 'all',
+                              multiple_tables=True, lattice = False,
+                              pandas_options={'header':None} 
+                              )
+except Exception as e: 
+    print('The error is', e)
 
-    #Cleaning up each page before joining them 
-    df = []
+df_list
 
-    for dfs in df_list:
-        dfs = dfs[dfs.columns[dfs.isnull().mean() < 0.8]]
-        #drop rows with empty cells
-        dfs.dropna(axis=0, how='any', thresh = 2, subset = None, inplace = True)
-        if dfs.shape[1] > 5:
-            dfs.drop(dfs.columns[-1], axis = 1, inplace = True)
-            df.append(dfs)
-        else:
-            df.append(dfs)  
+#Cleaning up each page before joining them 
+df = []
 
-    # Join individual dataframes into one
-    df_fin = pd.concat([df[1],df[2],df[3],df[4]], axis=0, sort=False) #FIX: make this part dynamic!
-    df_fin = df_fin[~df_fin.iloc[:,0].str.contains("Date")]
-    df_fin.columns = ['date',"trns_desc_1",'trns_type','amount','net_amount']
+for dfs in df_list:
+    dfs = dfs[dfs.columns[dfs.isnull().mean() < 0.8]]
+    #drop rows with empty cells
+    dfs.dropna(axis=0, how='any', thresh = 2, subset = None, inplace = True)
+    if dfs.shape[1] > 5:
+        dfs.drop(dfs.columns[-1], axis = 1, inplace = True)
+        df.append(dfs)
+    else:
+        df.append(dfs)  
 
-    #Adding missing column as row to data frame
-    df_fin.loc[len(df_fin.index)] = ['11/16/2021', 'Deposit', 'Deposit', '$3,200.49', '$3,200.49'] 
+dfs.sample()
 
-    return df_fin.to_csv('november_22_transactions.csv')
+# Join individual dataframes into one
+df_fin = pd.concat([df[1],df[2],df[3],df[4]], axis=0, sort=False) #FIX: make this part dynamic!
+df_fin = df_fin[~df_fin.iloc[:,0].str.contains("Date")]
+df_fin.columns = ['date',"trns_desc_1",'trns_type','amount','net_amount']
 
-create_df_convert_csv('november21.pdf')
+df_fin.to_csv('december_21_transactions.csv')
+
+
